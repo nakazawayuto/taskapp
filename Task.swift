f2207fbb74ad3238db15bd4d319fa26e160a1f22
中澤ゆうと <nakazawayuuto@nakazawayuutos-macbook-ea.local>
2018-11-28 15:58:25 +0900

    カリキュラム終了

---
+++ Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 中澤ゆうと on 2018/11/27.
+//  Copyright © 2018年 nakazawayuto. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
